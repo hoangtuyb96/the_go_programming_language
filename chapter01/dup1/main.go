3b370898c45cc81872427dacaddb13d215c36db8
hoangtuyb96 <hoangtuyb96@gmail.com>
2021-06-15 23:21:43 +0900

    Find dup

---
+++ chapter01/dup1/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	counts := make(map[string]int)
+	input := bufio.NewScanner(os.Stdin)
+
+	for input.Scan() {
+		counts[input.Text()]++
+		if input.Text() == "break" { break }
+	}
+
+	fmt.Println("Results of duplicate:")
+	for line, n := range counts {
+		if n > 1 {
+			fmt.Printf("%d\t%s\n", n, line)
+		}
+	}
+}
---
+++ chapter01/dup2/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	counts := make(map[string]int)
+	files := os.Args[1:]
+	if len(files) == 0 {
+		countLines(os.Stdin, counts)
+	} else {
+		for _, arg := range files {
+			// The function os.Open returns two values
+			// The first is an open file (*os.File) that is used in subsequent reads by the Scanner.
+			// The second result of os.Open is a value of the built-in error type
+			f, err := os.Open(arg)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "dup2: %v\n", err)
+				continue
+			}
+			countLines(f, counts)
+			f.Close()
+		}
+	}
+	
+	fmt.Println("Results of duplicate:")
+	for line, n := range counts {
+		if n > 1 {
+			fmt.Printf("%d\t%s\n", n, line)
+		}
+	}
+}
+
+func countLines(f *os.File, counts map[string]int) {
+	input := bufio.NewScanner(f)
+	for input.Scan() {
+		counts[input.Text()]++
+		if input.Text() == "break" { break }
+	}
+}
