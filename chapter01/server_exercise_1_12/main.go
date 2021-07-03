41c6a87031064fdc874aabf3ed1c4d35c0cbc91e
hoangtuyb96 <hoangtuyb96@gmail.com>
2021-07-03 18:15:02 +0900

    Doing server example and exercise

---
+++ chapter01/server_exercise_1_12/main.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"fmt"
+	"image"
+	"image/color"
+	"image/gif"
+	"io"
+	"log"
+	"math"
+	"math/rand"
+	"net/http"
+	"strconv"
+)
+
+var palette = []color.Color{color.White, color.Black}
+
+const (
+	whiteIndex = 0 // first color in palette
+	blackIndex = 1 // next color in palette
+)
+
+func main() {
+	http.HandleFunc("/", handler) // each request calls handler
+	log.Fatal(http.ListenAndServe("localhost:8000", nil))
+}
+
+// handler echoes the Path component of the requested URL.
+func handler(w http.ResponseWriter, r *http.Request) {
+	// default of cycles
+	var cycles = 5.0
+
+	if err := r.ParseForm(); err != nil {
+		log.Print(err)
+	}
+
+	cyclesForm := r.FormValue("cycles")
+
+	if cyclesForm != "" {
+		cycles, err := strconv.Atoi(cyclesForm)
+		if err != nil {
+			fmt.Fprintf(w, "bad cycles param: %s", cyclesForm, err)
+			return
+		}
+		lissajous(float64(cycles), w)
+		return
+	}
+
+	lissajous(cycles, w)
+}
+
+func lissajous(cycles float64, out io.Writer) {
+	const (
+		res     = 0.001 // angular resolution
+		size    = 100   // image canvas covers [-size..+size]
+		nframes = 64    // number of animation frames
+		delay   = 8     // delay between frames in 10ms units
+	)
+	freq := rand.Float64() * 3.0 // relative frequency of y oscillator
+	anim := gif.GIF{LoopCount: nframes}
+	phase := 0.0 // phase difference
+	for i := 0; i < nframes; i++ {
+		rect := image.Rect(0, 0, 2*size+1, 2*size+1)
+		img := image.NewPaletted(rect, palette)
+		for t := 0.0; t < cycles*2*math.Pi; t += res {
+			x := math.Sin(t)
+			y := math.Sin(t*freq + phase)
+			img.SetColorIndex(size+int(x*size+0.5), size+int(y*size+0.5),
+				blackIndex)
+		}
+		phase += 0.1
+		anim.Delay = append(anim.Delay, delay)
+		anim.Image = append(anim.Image, img)
+	}
+	gif.EncodeAll(out, &anim) // NOTE: ignoring encoding errors
+}
